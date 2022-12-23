f59af1b6b62b75266a31903b32df957da4d18558
jetakastrati <jetakastrati29@gmail.com>
2022-12-23 16:00:47 +0100

    fix

---
+++ backend/SMIS_API/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿namespace SMIS_API.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+           
+        public string Clas { get; set; }
+
+        public int ECTS { get; set; }
+
+
+    }
+}
---
+++ frontend/src/Pages/Subject/CreateSubject.jsx
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react';
+
+function CreateSubject() {
+
+    const [form, setForm] = useState({
+        id: "",
+        SubjectName: "",
+        Clas: "",
+        ECTS: "",
+      });
+    
+      function handleInputChange(event) {
+        const { name, value } = event.target;
+        setForm({ ...form, [name]: value });
+      }
+      function handleSubmit() {
+        const request = {
+          id: 0,
+          name : form.Name,
+          clas: form.Clas,
+          ects: form.ECTS,
+
+        };
+        axios
+          .post("https://localhost:7255/api/Subjects", request)
+          .then((res) => handleStatusCode(res.status));
+      }
+    
+      function handleStatusCode(statusCode) {
+        if (statusCode === 200) {
+          setForm({
+            id: "",
+            name: "",
+            clas: "",
+            ects: "",
+            
+          });
+        }
+      }
+    
+      
+      return (
+        <div className="container">
+          <header>
+            <h1>Create Subject</h1>
+          </header>
+          <div className="content">
+            <div className="form">
+              <h3>Subject Details</h3>
+    
+              <form className="form-details">
+                <div className="form-input">
+                  <label htmlFor="name">Subject Name</label>
+                  <input
+                    name="Name"
+                    type="text"
+                    value={form.Name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+    
+                <div className="form-input">
+                  <label htmlFor="name">Class</label>
+                  <input
+                    name="Clas"
+                    type="text"
+                    value={form.Clas}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-input">
+                  <label htmlFor="name">ECTS</label>
+                  <input
+                    name="ECTS"
+                    type="number"
+                    value={form.ECTS}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-input"></div>
+                <div className="form-input">
+                  <button className="button" onClick={handleSubmit}>
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    <div>CreateSubject</div>
+  
+}
+
+export default CreateSubject
