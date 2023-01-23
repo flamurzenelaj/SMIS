7b7dfcf8469f8ddc0514cf288a220d9d944f22fc
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-01-23 19:22:35 +0200

    Removed Components Folder

--- frontend/src/Components/CustomSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import ClipLoader from "react-spinners/ClipLoader";
-
-const CustomSpinner = ({ size = 50 }) => {
-  const color = "#C69A50";
-
-  return <ClipLoader color={color} size={size} />;
-};
-
-export default CustomSpinner;
--- frontend/src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as BurgerMenu } from './BurgerMenu';
-export { default as Typography } from './Typography/Typography';
-export { default as CustomSpinner } from './CustomSpinner';
