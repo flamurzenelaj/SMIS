4115a685c0df70468d0be16cb1ae6a60ab245a50
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-01-26 13:26:13 +0200

    Added some pics of campus

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -3,11 +3,18 @@ import infermiery from '../assets/infermiery.jpg';
 import datascience from '../assets/datascience.jpg';
 import user1 from '../assets/user1.jpg';
 import user2 from '../assets/user2.jpg';
+import london from '../assets/london.png';
+import newyork from '../assets/newyork.png';
+import washington from '../assets/washington.png';
+
 
 export default {
     computerscience,
     infermiery,
     datascience,
     user1,
-    user2
+    user2,
+    london,
+    washington,
+    newyork,
 };
